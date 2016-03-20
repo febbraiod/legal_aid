b7283f959742551b124327461907cbdafc6b7123
febbraiod <donedesign1@gmail.com>
2016-03-20 17:24:51 -0400

    bootstrapped some css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get '/' => 'static#home'
+  get '/about' => 'static#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
