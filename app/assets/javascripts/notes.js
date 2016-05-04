2d1492ffd4ffce6ce5e81a1679a7ef2659329c87
febbraiod <donedesign1@gmail.com>
2016-05-04 15:14:10 -0400

    rendering notes for a case in javascript works including edit button. need to move delete button to edit page

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -1,4 +1,5 @@
-function Note(content, user, date){
+function Note(id, content, user, date){
+  this.id = id;
   this.content = content;
   this.user = user;
   this.date = date;
@@ -9,28 +10,28 @@ Note.prototype.userFormat = function(){
   return fullName;
 };
 
-
-
 function getNotes(case_id){
   $.get('/notes', {case_id: case_id}).done(function(data){
       $('#case_notes').css('display', 'block');
       var notes = data.notes;
       for(var i = 0; i < notes.length ; i++){
-        var note = new Note(notes[i].content, notes[i].user, notes[i].created_at);
-        renderNote(note);
+        var note = new Note(notes[i].id, notes[i].content, notes[i].user, notes[i].created_at);
+        renderNote(note);  
       }
-      // loop through each note
-      // append info to DOM
   });
 }
 
 function renderNote(note){
-  debugger
   var noteString = "<p>"+ note.content + "</p>" +"<p>- " + 
   note.userFormat() + "<br><br>" + note.date + "</p>";
-  debugger
+  var edit_button = '<p><a href="/notes/' + note.id + '/edit">edit your note</a></p>';
   $('#case_notes').append(noteString);
+  $('#case_notes').append(edit_button);
+
+
 }
+
+
 
 function setBinders(){
   $('#getNotes').on('click', function(){
